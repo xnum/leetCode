99b5ecad0056af88600089588925c72c58bf72da
num <s000032001@gmail.com>
2015-12-08 10:10:10 +0800

    Excel Column

---
+++ esct.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string convertToTitle(int n) {
+	string res;
+	while( n > 0 ) {
+		int digit = --n % 26;
+		n /= 26;
+		res += (char)('A' + digit);
+	}
+
+	reverse(res.begin() ,res.end());
+
+	return res;
+}
+
+int main() {
+	int n;
+	while( cout << "Give me number:" , cin >> n )
+		cout << convertToTitle(n) << endl;
+	return 0;
+}
